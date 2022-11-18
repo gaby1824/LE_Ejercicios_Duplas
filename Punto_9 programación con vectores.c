31fda41b757cc2169439bc59f91e946209f85b23
gaby1824 <116888385+gaby1824@users.noreply.github.com>
2022-11-17 22:33:26 -0300

    Update Punto_9 programación con vectores.c

--- Punto_9 programación con vectores.c
+++ Punto_9 programación con vectores.c
@@ -7,11 +7,11 @@ int main()
     srand(time(NULL));
     
     int vect[10];
-    int min,max;
+    int min, max;
     
     printf("Ingrese el valor minimo y maximo\n");
-    scanf("%d",&min);
-    scanf("%d",&max);
+    scanf("%d", &min);
+    scanf("%d", &max);
     
     for (int i=0; i<10; i++){
         vect[i] = rand() % (max-min+1) + min;
