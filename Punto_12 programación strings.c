7b4e98871d2aab0bf79137c2989299f9ae677a76
gaby1824 <116888385+gaby1824@users.noreply.github.com>
2022-11-17 22:30:07 -0300

    Update Punto_12 programación strings.c

--- Punto_12 programación strings.c
+++ Punto_12 programación strings.c
@@ -1,10 +1,16 @@
 #include <stdio.h>
-#include <string.h>
+#define CARACTERES_MAX 50
 
-int main(void) {
-  char word[40];
-  printf("Ingrese una palabra: ");
-  scanf("%s",&word);
-  printf("La palabra tiene %d caracteres.", strlen(word));
-  return 0;
+int main() {
+    int i;
+    char palabra[CARACTERES_MAX];
+    printf("Ingrese una palabra:\n");
+    scanf("%s", palabra);
+    for (i = 0; i < CARACTERES_MAX; i++)
+    {
+        if (palabra[i] == '\0' ) break;
+        
+    }
+    printf("La palabra tiene %d caracteres.", i);
+    return 0;
 }
